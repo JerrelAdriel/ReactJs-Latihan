8a5b87d365d977880d07114dfa5b1628306a9c4c
JerrelAdriel <77767187+JerrelAdriel@users.noreply.github.com>
2022-05-21 01:00:43 +0700

    add simple about pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ return (
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,10 @@
-import { useState, useEffect} from 'react';
+import { Container } from 'react-bootstrap';
 
 function About() {
-    const [ count, setCount ] = useState(0);
-    const [ minus, setMinus ] = useState(100);
-
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
-    }, [count, minus]);
-
     return (
-        <div>
-            <h1>Ini About</h1>
-            <h1>Ini di klik sebanyak {count} kali</h1>
-            <button onClick= {() => setCount(count + 1)}>Klik</button>
-            <h1>Ini berkurang sebanyak {minus} kali</h1>
-            <button onClick= {() => setMinus(minus - 1)}>Klik</button>
-        </div>
+        <Container className='my-5 text-center bg-secondary py-5'>
+            Web ini merupakan sebuah website yang berisi tentang penjualan bundle valorant.
+        </Container>
     );
 }
 
